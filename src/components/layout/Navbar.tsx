07c820b745153ac8c672a892a7cb2ce08be6c82d
yunaduche <yunaducherte@gmail.com>
2026-05-25 15:19:37 +0300

    navbar auth handler

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,33 @@
 import { Link } from 'react-router-dom';
 import { Dumbbell } from 'lucide-react';
+import {Button} from '../ui/Button';
 
 export default function Navbar() {
+    const user = false;
     return <header className='fixed top-0 left-0 right-0 z-50 border-b border-[var(--color-border)] bg-[var(--color-background)]/80 backdrop-blur-md'>
-        <Link to="/">
-            <Dumbbell/> 
-            Bemanatry
-        </Link>
+        <div className='max-w-6xl mx-auto px-6 h-16 flex items-center justify-between'>
+            <Link to="/" className='flex items-center gap-2 text-[var(--color-foreground)] '>
+                <Dumbbell className='w-6 h-6 text-[var(--color-accent)]'/> 
+            <span className='font-bold text-lg'>Bemanatry</span>
+            </Link>
+            <nav>
+                {
+                user ? (<>
+                    <Link to="/profile">
+                    <   Button variant="ghost"  size="sm"> My Plan</Button>
+                    </Link>
+                </>) : 
+                (<>
+                    <Link to="/auth/sign-in">
+                        <Button variant="ghost"  size="sm"> Sign in</Button>
+                    </Link>
+                    <Link to="/auth/sign-up">
+                        <Button size="sm"> Sign up</Button>
+                    </Link>
+                </>)
+                }
+                
+            </nav>
+        </div>
     </header>
 }
